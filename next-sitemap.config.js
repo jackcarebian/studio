b694186ab8e4e13ed0e26eb5a61b4e8f2a215d23
jimmy tjahyono <jimmy.tjahyono@gmail.com>
2026-03-01 05:58:10 +0000

    Peta situs dapat dibaca, namun mengalami error Peta situs adalah HTML 1

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://jasa-pembuatan-website-aplikasi.vercel.app',
-  generateRobotsTxt: true, 
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
 }
